6dabf9b8340de2bc5323b149769250aa3018bdab
Sapna <Sapnakundnani@gmail.com>
2021-01-31 17:02:24 +0000

    This is my second commit

--- Demo1.java
+++ Demo1.java
@@ -1,8 +1,9 @@
 class Demo1
 {
-	publicstatic void main(String args[])
+	public static void main(String args[])
 	{
 		System.out.println("Demo 1 File");
+		System.out.println("adding Line 2 after first commit");
 	}
 }
 
